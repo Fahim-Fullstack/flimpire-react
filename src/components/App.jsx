d2999e558951d21046acc2acd625f3f4f1a4ef5a
Fahim <fhasan5@wgu.edu>
2023-08-14 13:50:16 -0500

    adding file and structure

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,28 @@
 import React from 'react';
 import { CssBaseline } from '@mui/material';
-// import { Route, Switch } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
+import useStyles from './styles';
+
+import { MovieInformation, Movies, NavBar, Profile, Actors } from '.';
 
 const App = () => {
-  <div>
-    <CssBaseline />
-    <h1>Hello, world, </h1>
-  </div>;
+  const classes = useStyles();
+  return (
+    <div className={classes.root}>
+      <CssBaseline />
+      <NavBar />
+      <main className={classes.content}>
+        <div className={classes.toolbar} />
+        <Routes>
+          {/* <Route path='/' element={<Home />} /> */}
+          <Route path='/' element={<Movies />} />
+          <Route path='/movies/:id' element={<MovieInformation />} />
+          <Route path='/actors/:id' element={<Actors />} />
+          <Route path='/profile/:id' element={<Profile />} />
+        </Routes>
+      </main>
+    </div>
+  );
 };
 
 export default App;
