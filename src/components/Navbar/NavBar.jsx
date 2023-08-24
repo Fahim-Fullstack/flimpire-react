2b6cf2378f333ff3333aaa3ad7b3da10d01c9fc1
Fahim <fhasan5@wgu.edu>
2023-08-24 12:40:28 -0500

    file and folder

--- src/components/Navbar/NavBar.jsx
+++ src/components/Navbar/NavBar.jsx
@@ -1,5 +1,41 @@
 import React from 'react';
+import {
+  AppBar,
+  IconButton,
+  Toolbar,
+  Drawer,
+  Avatar,
+  Button,
+  useMediaQuery,
+} from '@mui/material';
+import {
+  Menu,
+  AccountCircle,
+  Brightness4,
+  Brightness7,
+} from '@mui/icons-material';
+import { Link } from 'react-router-dom';
+import useStyles from './styles';
 
-const NavBar = () => <div>NavBar</div>;
+const NavBar = () => {
+  const classes = useStyles();
+  const isMobile = useMediaQuery('(Max-width: 600px)');
+  return (
+    <AppBar position='fixed'>
+      <Toolbar className={classes.toolbar} />
+      {isMobile && (
+        <IconButton
+          color='inherit'
+          edge='start'
+          style={{ outline: 'none' }}
+          onClick={() => {}}
+          className={classes.menuButton}
+        >
+          <Menu />
+        </IconButton>
+      )}
+    </AppBar>
+  );
+};
 
 export default NavBar;
